2a2fe8d6b9c7e1c9b68057604ab617402d1b16c2
alirazamr951-jpg <alirazamr951@gmail.com>
2026-03-13 20:33:09 -0700

    dropdown icon updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const selectArea = document.querySelector("#selectArea");
 const optionn = document.querySelectorAll(".options");
 const showBtn = document.querySelector("#btn1");
 const optinBox = document.querySelector("#option-group");
+const showIcon = document.querySelector(".show-icon");
 optionn.forEach((optionn) => {
     optionn.addEventListener("click", () => {
         selectArea.innerText = optionn.innerText;
@@ -11,10 +12,11 @@ let turn=true;
 optinBox.style.display = "none";
 showBtn.addEventListener("click", () => {
     if (turn == true){
-        showBtn.innerText = "Hide";
+        showIcon.className = "show-icon-b";
     optinBox.style.display = "flex";
     turn=false;
 }else{
+    showIcon.className = "show-icon";
     optinBox.style.display = "none";
     turn=true;
 }
